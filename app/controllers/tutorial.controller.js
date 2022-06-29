cd41b4e64fd71960ffc08c0810177b8036179279
Chris5477 <petitcolaschristopher@gmail.com>
2022-06-29 21:01:36 +0200

    remove regex and search by title with sensible case OK

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -28,7 +28,7 @@ exports.create = (req, res) => {
 // Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
 	const title = req.query.title;
-	var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+	var condition = title ? { title: title } : {};
 	Tutorial.find(condition)
 		.then((data) => {
 			res.send(data);
